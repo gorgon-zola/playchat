c75abdd6d756bd529d436e6dc86f8e6e25b013fe
cheddar <nezahrish@gmail.com>
2013-10-07 15:30:25 +0900

    add lib dependency 'pac4j'

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,17 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "mysql" % "mysql-connector-java" % "5.1.21", jdbc, anorm
+      "mysql" % "mysql-connector-java" % "5.1.21", jdbc, anorm,
+      "org.pac4j" % "play-pac4j_scala2.10" % "1.1.1",
+      "org.pac4j" % "pac4j-http" % "1.4.2-SNAPSHOT",
+      "org.pac4j" % "pac4j-cas" % "1.4.2-SNAPSHOT",
+      "org.pac4j" % "pac4j-openid" % "1.4.2-SNAPSHOT",
+      "org.pac4j" % "pac4j-oauth" % "1.4.2-SNAPSHOT"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
-      testOptions in Test += Tests.Argument("junitxml", "console")
+      testOptions in Test += Tests.Argument("junitxml", "console"),
+      resolvers += "Sonatype snapshots repository" at "https://oss.sonatype.org/content/repositories/snapshots/"
     )
-
 }
